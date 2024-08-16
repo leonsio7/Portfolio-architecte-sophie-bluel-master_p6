c4cf2989fdb9660dde03aef1ce58ee3835314033
leonsio7 <levanbatmanishvili@gmail.com>
2024-08-16 17:22:32 +0200

    commit2

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,77 @@
 let works = [];
+let categories = [];
+
 const gallery = document.querySelector('.gallery')
+const filtres = document.querySelector('.filtres');
 
-fetch("http://localhost:5678/api/works")
-.then((rep) => rep.json())
-.then((rep) => {
-    works = rep;
-    displaywork();
-})
+const getWorks = () => {
+    fetch("http://localhost:5678/api/works")
+        .then((rep) => rep.json())
+        .then((rep) => {
+            works = rep;
+            displayWork();
+        })
 
-const displaywork =() => {
-    gallery.innerHTML ="";
-    
-   
-    works.forEach((element) =>{
+}
+
+getWorks();
+
+const displayWork = () => {
+    gallery.innerHTML = "";
+
+
+    works.forEach((element) => {
         gallery.innerHTML +=
-        `<figure>
+            `<figure>
 				<img src=${element.imageUrl} alt="Abajour Tahina">
 				<figcaption>${element.title}</figcaption>
 			</figure>`
+
+    })
+}
+
+const getCategories = () => {
+    fetch("http://localhost:5678/api/categories")
+        .then((rep) => rep.json())
+        .then((rep) => {
+            categories = rep;
+            displayFiltres();
+        })
+
+}
+
+getCategories();
+
+const displayFiltres = () => {
+    const BtnTous = document.createElement("Button");
+    BtnTous.innerText = "Tous";
+    BtnTous.addEventListener('click', () => {
+        displayWork();
+    })
+    filtres.appendChild(BtnTous);
+
+
+
+    categories.forEach((Cat) => {
+        const BtnFiltre = document.createElement("Button");
+        BtnFiltre.innerText = Cat.name;
+
+        BtnFiltre.addEventListener('click', () => {
+            gallery.innerHTML = "";
+
+            works.forEach((element) => {
+
+                if (Cat.id === element.categoryId) {
+                    gallery.innerHTML +=
+                        `<figure>
+				<img src=${element.imageUrl} alt="Abajour Tahina">
+				<figcaption>${element.title}</figcaption>
+			</figure>`
+                }
+            })
+        })
+
+        filtres.appendChild(BtnFiltre);
 
     })
 }
